544a7f010cc5d23214c33eed68255f634739474a
Mason Baldwin <me.baldwin1016@icloud.com>
2025-09-16 18:13:23 -0400

    Init commit

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-public class Part_1
+public class Main
 {
 
     public static void main(String[] args)
--- src/Main2.java
+++ src/Main2.java
@@ -1,4 +1,4 @@
-public class Part_1_continued
+public class Main2
 {
 
     public static void main(String[] args)
