e497c06f2408116754aade7b69397f01ae38f271
Mason Baldwin <me.baldwin1016@icloud.com>
2025-09-16 18:08:29 -0400

    Lab 04

---
+++ src/Task_2.java
@@ -0,0 +1,19 @@
+public class Task_2
+{
+    public static void main(String[] args)
+    {
+        double winterCost = 200;
+        double springCost = 200;
+        double summerCost = 200;
+        double fallCost = 200;
+
+        System.out.println("The winter cost is: " + winterCost);
+        System.out.println("The spring cost is: " + springCost);
+        System.out.println("The summer cost is: " + summerCost);
+        System.out.println("The fall cost is: " + fallCost);
+
+        double totalCost = winterCost + springCost + summerCost + fallCost;
+
+        System.out.println("The total yearly cost is: " + totalCost);
+    }
+}
