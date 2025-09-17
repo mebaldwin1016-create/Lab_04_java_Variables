e33e2a73ba60fdc225774fd12639a0dc369d68c7
Mason Baldwin <me.baldwin1016@icloud.com>
2025-09-16 20:14:38 -0400

    Task 2

--- src/Task_2.java
+++ src/Task_2.java
@@ -6,13 +6,14 @@ public class Task_2
         double springCost = 200;
         double summerCost = 200;
         double fallCost = 200;
+        double totalCost = 0;
 
         System.out.println("The winter cost is: " + winterCost);
         System.out.println("The spring cost is: " + springCost);
         System.out.println("The summer cost is: " + summerCost);
         System.out.println("The fall cost is: " + fallCost);
 
-        double totalCost = winterCost + springCost + summerCost + fallCost;
+        totalCost = winterCost + springCost + summerCost + fallCost;
 
         System.out.println("The total yearly maintenance cost is: " + totalCost);
     }
